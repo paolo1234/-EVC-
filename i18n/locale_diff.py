db08dca37137a849e36bd2ba86d32172878a4f28
Paolo Calamia <paolocalamia49@gmail.com>
2023-07-08 02:52:40 +0200

    Update locale_diff.py

--- i18n/locale_diff.py
+++ i18n/locale_diff.py
@@ -3,7 +3,7 @@ import os
 from collections import OrderedDict
 
 # Define the standard file name
-standard_file = "zh_CN.json"
+standard_file = "it_SG.json"
 
 # Find all JSON files in the directory
 dir_path = "./"
